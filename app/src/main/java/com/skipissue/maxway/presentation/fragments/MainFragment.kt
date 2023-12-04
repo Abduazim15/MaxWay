8c33810e1bfe57d500aa7d119f9fbe93cafe12a4
Abduazim <abduazim>
2023-12-04 19:51:57 +0500

    Container

---
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.skipissue.maxway.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import com.skipissue.maxway.R
+class MainFragment : Fragment(R.layout.main_fragment) {
+
+}
--- app/src/main/java/com/skipissue/maxway/presentation/fragments/SplashFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/SplashFragment.kt
@@ -1,6 +1,21 @@
 package com.skipissue.maxway.presentation.fragments
 
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.skipissue.maxway.R
 
-class SplashFragment : Fragment() {
+class SplashFragment : Fragment(R.layout.splash_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        object : CountDownTimer(1000, 2000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                parentFragmentManager.beginTransaction().replace(R.id.container, MainFragment()).commit()
+            }
+        }
+    }
 }
