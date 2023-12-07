252e5ea189bb495ab70774360e4d27fb8ff717d0
Abduazim <abduazim>
2023-12-07 13:30:38 +0500

    Item And Api

--- app/src/main/java/com/skipissue/maxway/data/api/MaxWayAPI.kt
+++ app/src/main/java/com/skipissue/maxway/data/api/MaxWayAPI.kt
@@ -1,4 +1,14 @@
 package com.skipissue.maxway.data.api
 
-class MaxWayAPI {
+import com.skipissue.maxway.domain.entity.responses.ProductsDetailResponse
+import com.skipissue.maxway.domain.entity.responses.ProductsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MaxWayAPI {
+    @GET("v1/core/get/category-with-products/")
+    suspend fun getProducts() : Response<ProductsResponse>
+
+    @GET("v1/core/get/product-detail/ed7654f9-a6e5-45a9-b8a0-9f150610cc74/?format=json")
+    suspend fun getProductsWith() : Response<ProductsDetailResponse>
 }
---
+++ app/src/main/java/com/skipissue/maxway/modules/ApplicationModules.kt
@@ -0,0 +1,4 @@
+package com.skipissue.maxway.modules
+
+class ApplicationModules {
+}
