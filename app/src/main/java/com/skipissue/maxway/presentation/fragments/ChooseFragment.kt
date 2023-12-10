c2096cd9923c7a924c571536771b08f0f8b99ffb
Abduazim <abduazim>
2023-12-11 00:55:09 +0500

    choose_fragment.xml

--- app/src/main/java/com/skipissue/maxway/presentation/fragments/ChooseFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/ChooseFragment.kt
@@ -1,7 +1,25 @@
 package com.skipissue.maxway.presentation.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.skipissue.maxway.R
-class ChooseFragment : Fragment(R.layout.choose_fragment) {
+import com.skipissue.maxway.databinding.ChooseFragmentBinding
 
+class ChooseFragment : Fragment(R.layout.choose_fragment) {
+    private val binding : ChooseFragmentBinding by viewBinding()
+    private var counter = 1
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.dec.setOnClickListener {
+            if (counter != 1){
+                counter--
+                binding.amount.setText(counter.toString())
+            }
+        }
+        binding.inc.setOnClickListener {
+            counter++
+            binding.amount.setText(counter.toString())
+        }
+    }
 }
