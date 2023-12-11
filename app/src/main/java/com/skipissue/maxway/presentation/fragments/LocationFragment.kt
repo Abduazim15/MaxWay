5beffe0eddf12e2d7f50e763475faa19c1c9172b
Abduazim <abduazim>
2023-12-11 16:10:05 +0500

    location_fragment.xml

--- app/src/main/java/com/skipissue/maxway/presentation/fragments/LocationFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/LocationFragment.kt
@@ -1,17 +1,20 @@
 package com.skipissue.maxway.presentation.fragments
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.skipissue.maxway.R
+import com.skipissue.maxway.databinding.LocationFragmentBinding
 import com.yandex.mapkit.MapKitFactory
 
 class LocationFragment : Fragment(R.layout.location_fragment) {
+    private val binding: LocationFragmentBinding by viewBinding()
     override fun onStart() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
-        mapView.onStart()
+        binding.mapView.onStart()
     }
     override fun onStop() {
-        mapView.onStop()
+        binding.mapView.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
