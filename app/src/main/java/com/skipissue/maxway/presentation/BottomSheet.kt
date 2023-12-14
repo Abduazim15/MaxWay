98b9f8e2c3ea24a19020a2bd30f157d989096360
Abduazim <abduazim>
2023-12-14 20:14:24 +0500

    bottom_sheet

---
+++ app/src/main/java/com/skipissue/maxway/presentation/BottomSheet.kt
@@ -0,0 +1,10 @@
+package com.skipissue.maxway.presentation
+
+import android.app.Dialog
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.skipissue.maxway.R
+
+class BottomSheet : BottomSheetDialogFragment(R.layout.language_bottom_sheet) {
+}
--- app/src/main/java/com/skipissue/maxway/presentation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/SettingsFragment.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.skipissue.maxway.MainActivity
 import com.skipissue.maxway.R
 import com.skipissue.maxway.databinding.SettingsFragmentBinding
+import com.skipissue.maxway.presentation.BottomSheet
 
 class SettingsFragment: Fragment(R.layout.settings_fragment){
     private val binding: SettingsFragmentBinding by viewBinding()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        val bottomsheet = BottomSheet()
+        binding.language.setOnClickListener {
+            bottomsheet.show(parentFragmentManager, "language")
+        }
     }
 
     override fun onDestroy() {
