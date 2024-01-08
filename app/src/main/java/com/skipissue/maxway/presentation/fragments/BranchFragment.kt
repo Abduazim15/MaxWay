bb967352c42cd21d0edb62b378fe180e67aeda34
Abduazim <abduazim>
2024-01-08 16:48:46 +0500

    Branches and set

--- app/src/main/java/com/skipissue/maxway/presentation/fragments/BranchFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/BranchFragment.kt
@@ -1,8 +1,31 @@
 package com.skipissue.maxway.presentation.fragments
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.skipissue.maxway.R
+import com.skipissue.maxway.databinding.BranchFragmentBinding
 
-class BranchFragment : Fragment(R.layout.branch_fargment) {
+class BranchFragment : Fragment(R.layout.branch_fragment) {
+
+    private val binding: BranchFragmentBinding by viewBinding()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            locationText.setText(arguments?.getString("name"))
+            orientorText.setText(arguments?.getString("orient"))
+            timeText.setText("${arguments?.getString("timeStart")} - ${arguments?.getString("timeEnd")}")
+            phoneText.setText(ProfileFragment().formatPhoneNumber(arguments?.getString("phone")!!))
+            phone.setOnClickListener {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:${phoneText.text}")
+                startActivity(intent)
+            }
+            back.setOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+        }
+
+    }
 
 }
--- app/src/main/java/com/skipissue/maxway/presentation/fragments/BranchesFragment.kt
+++ app/src/main/java/com/skipissue/maxway/presentation/fragments/BranchesFragment.kt
@@ -3,6 +3,7 @@ package com.skipissue.maxway.presentation.fragments
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -32,9 +33,17 @@ class BranchesFragment : Fragment(R.layout.branches_fragment) {
             }
         }
         adapter.setOnClickClickListener { index ->
-            parentFragmentManager.beginTransaction().addToBackStack("BranchesFragment").replace(R.id.container, BranchFragment()).commit()
+            parentFragmentManager.beginTransaction().addToBackStack("BranchesFragment")
+                .replace(R.id.container, BranchFragment::class.java, bundleOf(
+                    "name" to adapter.currentList[index].address,
+                    "orient" to adapter.currentList[index].destination,
+                    "timeStart" to adapter.currentList[index].work_hour_start,
+                    "timeEnd" to adapter.currentList[index].work_hour_end,
+                    "phone" to adapter.currentList[index].phone,
+                )).commit()
         }
     }
+
     override fun onStart() {
         super.onStart()
         (requireActivity() as MainActivity).hideOrShow(true)
